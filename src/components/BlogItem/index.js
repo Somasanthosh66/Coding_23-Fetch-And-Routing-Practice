2a9529e78b06800e73de58e5efada0a7cba25d8e
Somasanthosh66 <santhoshsoma75@gmail.com>
2024-03-26 18:10:34 +0000

    FetchAndRouting Practice solution

---
+++ src/components/BlogItem/index.js
@@ -0,0 +1,28 @@
+// Write your JS code here
+import {Link} from 'react-router-dom'
+
+import './index.css'
+
+const BlogItem = props => {
+  const {blogData} = props
+  const {id, title, imageUrl, avatarUrl, author, topic} = blogData
+  return (
+    <li className="blog-item">
+      <Link to={`/blogs/${id}`} className="blog-item-link">
+        <div className="item-container">
+          <img src={imageUrl} alt={`item${id}`} className="item-image" />
+          <div className="item-info">
+            <p className="item-topic">{topic}</p>
+            <h1 className="item-title">{title}</h1>
+            <div className="author-info">
+              <img className="avatar" src={avatarUrl} alt={`avatar${id}`} />
+              <p className="author-name">{author}</p>
+            </div>
+          </div>
+        </div>
+      </Link>
+    </li>
+  )
+}
+
+export default BlogItem
